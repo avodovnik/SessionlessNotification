8c34e451b2699b2068ce95c7481214475acb935a
Anže Vodovnik <anze@vodovnik.com>
2012-07-11 18:18:19 +0200

    The implementation of the example.

--- SessionlessNotification.Web/Controllers/HomeController.cs
+++ SessionlessNotification.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SessionlessNotification.Web.App_Start;
 
 namespace SessionlessNotification.Web.Controllers
 {
@@ -16,5 +17,15 @@ namespace SessionlessNotification.Web.Controllers
             return View();
         }
 
+        public ActionResult Post()
+        {
+            SessionlessNotification.Web.App_Start.Notifications.NotificationModule.AddNotification(new Notifications.NotificationModule.NotificationMessage()
+                                                                                                       {
+                                                                                                           Message = "This is a notification.",
+                                                                                                           Type = "info"
+                                                                                                       });
+
+            return View();
+        }
     }
 }
